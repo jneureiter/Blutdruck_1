7e817331ecf6d4267e301827ebe3dc5f9f619c2f
Josef Neureiter <josef.neu05@gmail.com>
2020-11-23 08:09:48 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blutdruck_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
